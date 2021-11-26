85921eed9bf313ed7e1cca88d136fc0df56f5f12
pablo <pgnieto@paradigmadigital.com>
2021-11-26 12:24:33 +0100

    fifth exercise

---
+++ src/app/home/lessons-list/lessons-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-lessons-list',
+  templateUrl: './lessons-list.component.html',
+  styleUrls: ['./lessons-list.component.scss']
+})
+export class LessonsListComponent {
+  @Input() title;
+  @Input() courseLessons;
+  @Output() selected = new EventEmitter();
+}
